f2aeff3ea6a445ec2d90968ad486e89b2fa1204e
sinchana-kumbale <77568895+sinchana-kumbale@users.noreply.github.com>
2024-02-02 10:14:44 +0530

    Update get_DEPTWEET_details.py

--- get_DEPTWEET_details.py
+++ get_DEPTWEET_details.py
@@ -2,10 +2,13 @@
 import pandas as pd
 import numpy as np
 
-df = pd.read_csv('deptweet_dataset.csv')
-#Displaying number of tweets for each label
-print(df['label'].value_counts())
-#Finding average confidence_score for each label
-labels = set(df['label'])
-for label in labels:
-    print(label,df.loc[df['label'].eq(label),'confidence_score'].mean())
+def get_dataset_details(filename):
+    df = pd.read_csv(filename)
+    #Displaying number of tweets for each label
+    print(df['label'].value_counts())
+    #Finding average confidence_score for each label
+    labels = set(df['label'])
+    for label in labels:
+        print(label,df.loc[df['label'].eq(label),'confidence_score'].mean())
+if __name__ == 'main':
+    get_dataset_details('deptweet_dataset.csv')
