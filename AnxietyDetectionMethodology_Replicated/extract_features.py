8f80e111971eedb42e5bf4f0d8845239218dcd7c
sinchana-kumbale <77568895+sinchana-kumbale@users.noreply.github.com>
2024-02-16 16:56:29 +0530

    Add preprocessing pipeline to extract_features.py

--- AnxietyDetectionMethodology_Replicated/extract_features.py
+++ AnxietyDetectionMethodology_Replicated/extract_features.py
@@ -4,12 +4,72 @@ import os
 import glob
 from fairseq.models.wav2vec import Wav2VecModel
 from fairseq.models.roberta import RobertaModel
+from nltk.corpus import stopwords
+import re
 import tqdm
 import librosa
 import math
 import numpy as np
 import pandas as pd
 import pickle
+
+
+stop_words = set(stopwords.words('english'))
+def remove_stopwords(text):
+    new_sentence = []
+    list_of_words = text.split()
+    for word in list_of_words:
+        if word not in stop_words:
+            new_sentence.append(word)
+    return ' '.join(new_sentence)
+
+def clean_text(text):
+    """
+    Cleans text by removing unnecessary characters, expanding contractions,
+  and handling special cases.
+  """
+    clean_text_regex = r"[^A-Za-z0-9\s\!\?\+\-\=]"
+    text = re.sub(clean_text_regex, " ", text)
+    
+    # Replace contractions with expanded forms
+    contractions = {
+      "what's": "what is",
+      "\'s": " ",
+      "\'ve": " have",
+      "can't": "cannot",
+      "n't": " not",
+      "i'm": "i am",
+      "\'re": " are",
+      "\'d": " would",
+      "\'ll": " will",
+    }
+    for contraction, expansion in contractions.items():
+        text = text.replace(contraction, expansion)
+    
+    # Fix punctuation spacing and special characters
+    text = re.sub(r"([\!\?\+\-\=])", r" \1 ", text)
+    text = text.replace(" eg ", " eg ")
+    text = text.replace(" b g ", " bg ")
+    text = text.replace(" u s ", " american ")
+    text = text.replace("\0s", "0")
+    text = text.replace(" 9 11 ", "911")
+    text = text.replace("e - mail", "email")
+    text = text.replace("j k", "jk")
+    
+    # Remove extra spaces
+    text = re.sub(r"\s{2,}", " ", text)
+    
+    return text
+def preprocess_text(text):
+    # Convert to lowercase
+    text = text.lower()
+    
+    # Remove stopwords
+    # text = remove_stopwords(text)
+    
+    text = clean_text(text)
+    
+    return text
 
 roberta = torch.hub.load('pytorch/fairseq', 'roberta.large')
 roberta.eval()
@@ -28,8 +88,9 @@ with torch.no_grad():
         for index, row in df.iterrows():
             if row['speaker'] != 'Ellie':
                 text += str(row['value'])
-        tokens = roberta.encode(text)[:512]
-        pickle.dump(tokens, open('transcripts_tokens' + fname_tok, 'wb') )
-        last_layer_features = roberta.extract_features(tokens)
-#         print(last_layer_features.shape)
-        pickle.dump(last_layer_features, open('transcripts_features' + fname_feat, 'wb') )
+                text = preprocess_text(text)
+                tokens = roberta.encode(text)[:512]
+                pickle.dump(tokens, open('transcripts_tokens' + fname_tok, 'ab') )
+                last_layer_features = roberta.extract_features(tokens)
+                pickle.dump(last_layer_features, open('transcripts_features' + fname_feat, 'ab') )
+        
